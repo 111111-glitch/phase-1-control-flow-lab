28327590fca418b6860bcc38b032d7a4af914f95
Melissa <melissa.wanjiku@student.moringaschool.com>
2023-12-16 00:22:48 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if(x <= 400){
+    return "This one is on me!"
+  }
+  else if(x > 2000 && x <= 2500){
+    return "Iwill gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
 }
+
+
+const y = "NYC";
 
 function ternaryCheckCity(){
   // Write your code here!
+  return (y === "NYC") ? "Ok, sounds good" : "No go";
+
 }
 
 function switchOnCharmFromTip(){
+  
+  
   // Write your code here!
+}
+function switchOnCharmFromTip(){
+  switch(z){
+    case "generous" :
+      return ("Thank you so much.")
+      case "not generous":
+        return ("Thank you.")
+        default:
+          return("bye")
+  }
 }
